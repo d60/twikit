f17775c74ce3881cfaf2911128cb20503c825f43
d60 <94234663+d60@users.noreply.github.com>
2024-01-31 20:02:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
     name='twikit',
     version='1.0.1',
-    install_requires=['httpx', 'sphinx_rtd_theme '],
+    install_requires=['httpx', 'sphinx_rtd_theme'],
     description='Twitter API wrapper for python with no API key required.',
     long_description=long_description,
     long_description_content_type='text/markdown'
