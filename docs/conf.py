73279ad643a6405a5d5ea528b4d34c76d4a64a51
d60 <94234663+d60@users.noreply.github.com>
2024-02-02 21:03:19 +0900

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for todo extension ----------------------------------------------
