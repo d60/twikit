39f73d882de131649bade08fc5e6a8c7f859cc34
Scott <2660603+ShoGinn@users.noreply.github.com>
2024-04-25 13:24:05 -0400

    chore: use __all__ for init

--- twikit/__init__.py
+++ twikit/__init__.py
@@ -10,9 +10,23 @@ __version__ = '1.5.8'
 
 from .bookmark import BookmarkFolder
 from .client import Client
-from .community import (Community, CommunityCreator, CommunityMember,
-                        CommunityRule)
-from .errors import *
+from .community import Community, CommunityCreator, CommunityMember, CommunityRule
+from .errors import (
+    TwitterException,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    RequestTimeout,
+    TooManyRequests,
+    ServerError,
+    CouldNotTweet,
+    DuplicateTweet,
+    TweetNotAvailable,
+    InvalidMedia,
+    UserNotFound,
+    UserUnavailable,
+)
 from .group import Group, GroupMessage
 from .list import List
 from .message import Message
@@ -21,3 +35,38 @@ from .trend import Trend
 from .tweet import CommunityNote, Poll, ScheduledTweet, Tweet
 from .user import User
 from .utils import build_query
+
+__all__ = [
+    'BookmarkFolder',
+    'Client',
+    'Community',
+    'CommunityCreator',
+    'CommunityMember',
+    'CommunityRule',
+    'Group',
+    'GroupMessage',
+    'List',
+    'Message',
+    'Notification',
+    'Trend',
+    'CommunityNote',
+    'Poll',
+    'ScheduledTweet',
+    'Tweet',
+    'User',
+    'build_query',
+    'TwitterException',
+    'BadRequest',
+    'Unauthorized',
+    'Forbidden',
+    'NotFound',
+    'RequestTimeout',
+    'TooManyRequests',
+    'ServerError',
+    'CouldNotTweet',
+    'DuplicateTweet',
+    'TweetNotAvailable',
+    'InvalidMedia',
+    'UserNotFound',
+    'UserUnavailable',
+]
