6a5f17fb8e71535c18edff87ac4d7d2d477f8c05
d60 <kaweco2007@outlook.jp>
2024-05-02 04:26:02 +0900

    Updated __init__.py

--- twikit/__init__.py
+++ twikit/__init__.py
@@ -6,7 +6,7 @@ Twikit Twitter API Wrapper
 A Python library for interacting with the Twitter API.
 """
 
-__version__ = '1.5.12'
+__version__ = '1.5.13'
 
 from .bookmark import BookmarkFolder
 from .client import Client
